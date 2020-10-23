2f2cefcae4b2d6ce3f1e817c5e4c41b88ff9d173
David Sterry <sterry.david@gmail.com>
2020-10-23 11:09:07 -0700

    debug js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
-    const questOne =parseInt($("#morning").val());
-    const questTwo =parseInt($("apples").val());
-    const questThree =parseInt($("#potter").val());
-    const questFour =parseInt($("#animal").val());
-    const questFive =parseInt($("#coffee").val());
-    const results =$(".results");
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const questOne = parseInt($("#morning").val());
+    const questTwo = parseInt($("#apples").val());
+    const questThree = parseInt($("#potter").val());
+    const questFour = parseInt($("#animal").val());
+    const questFive = parseInt($("#coffee").val());
+    const results = $(".results");
 
-  event.preventDefault();
-  });
-});
+    if (questOne === "1"){
+      $(".results").children().hide();
+      $("#Java").show();
+    } else if (questTwo === "1"){
+      $(".reults").children().hide();
+      $("Swift").show();
+    } else if (questFive === "1"){
+      $(".results").children().hide();
+      $("#ruby").show();
+    }
+
+  })
+})
