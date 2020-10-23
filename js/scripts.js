57c284a4972e43f7a3b7c9af6ee46facccc73a9f
David Sterry <sterry.david@gmail.com>
2020-10-23 13:01:50 -0700

    fix if else statement, add more details to hmtl results

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
     event.preventDefault();
     const questOne = parseInt($("#morning").val());
     const questTwo = parseInt($("#apples").val());
-    const questThree = parseInt($("#potter").val());
-    const questFour = parseInt($("#animal").val());
     const questFive = parseInt($("#coffee").val());
     const results = $(".results");
 
-    if (questOne === "1" && questTwo === "1" || "2" && questThree === "1" || "2" && questFour === "1" || "2" && questFive === "1" || "2"){
+    if (questOne == "1" && questTwo == "2" && questFive == "2"){
       $(".results").children().hide();
       $("#Java").show();
-    } else if (questTwo === "1" && questThree === "1" || "2" && questFour === "1" || "2" && questFive === "1" || "2"){
+    } else if (questTwo == "1" && questOne == "2" && questFive == "2"){
       $(".results").children().hide();
-      $("Swift").show();
-    } else if (questFive === "1"){
+      $("#Swift").show();
+    } else if (questFive == "1" && questOne == "2" && questTwo == "2"){
       $(".results").children().hide();
-      $("#ruby").show();
+      $("#Ruby").show();
+    } else {
+      results.text("Hmmm you're a tricky one, maybe you'd be happier doing something else?")
     }
-
+ 
   })
 })
