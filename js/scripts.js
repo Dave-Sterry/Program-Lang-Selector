9f2d12d11dd5aa455895ed7b7059ddaec1fcacc4
David Sterry <sterry.david@gmail.com>
2020-10-23 09:08:29 -0700

    add basic Jquery to start

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  const quest1 = $("input:radio[name=question1]:checked").val();
+  const quest2 = $("input:radio[name=question2]:checked").val();
+  const quest3 = $("input:radio[name=question3]:checked").val();
+  const quest4 = $("input:radio[name=question4]:checked").val();
+  const quest5 = $("input:radio[name=question5]:checked").val();
+  const results =(".results");
+
+  if (ques1 === "yes" && quest2 === "no" )
+});
