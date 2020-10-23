76ff2be222813ffe41689d68abad0ee8e37eb8f4
David Sterry <sterry.david@gmail.com>
2020-10-23 09:52:32 -0700

    trouble shoot scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-  const quest1 = $("input:radio[name=question1]:checked").val();
-  const quest2 = $("input:radio[name=question2]:checked").val();
-  const quest3 = $("input:radio[name=question3]:checked").val();
-  const quest4 = $("input:radio[name=question4]:checked").val();
-  const quest5 = $("input:radio[name=question5]:checked").val();
-  const results =(".results");
+  $("formOne").submit(function(event) {
+    const quest1 = $("input:radio[name=question1]:checked").val();
+    const quest2 = $("input:radio[name=question2]:checked").val();
+    const quest3 = $("input:radio[name=question3]:checked").val();
+    const quest4 = $("input:radio[name=question4]:checked").val();
+    const quest5 = $("input:radio[name=question5]:checked").val();
+    const results =$(".results");
 
-  if (ques1 === "yes" && quest2 === "no" )
+    if (ques1 === "yes"){
+     results.text("You should learn Javascript!");
+    }
+
+  event.preventDefault();
+  });
 });
