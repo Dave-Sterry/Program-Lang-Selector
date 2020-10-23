cdf0a70497471e44fd9b062865054e8cc03b53de
David Sterry <sterry.david@gmail.com>
2020-10-23 10:16:00 -0700

    switch radio buttons to drop down forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
   $("formOne").submit(function(event) {
-    const quest1 = $("input:radio[name=question1]:checked").val();
-    const quest2 = $("input:radio[name=question2]:checked").val();
-    const quest3 = $("input:radio[name=question3]:checked").val();
-    const quest4 = $("input:radio[name=question4]:checked").val();
-    const quest5 = $("input:radio[name=question5]:checked").val();
+    const questOne =parseInt($("#morning").val());
+    const questTwo =parseInt($("apples").val());
+    const questThree =parseInt($("#potter").val());
+    const questFour =parseInt($("#animal").val());
+    const questFive =parseInt($("#coffee").val());
     const results =$(".results");
-
-    if (ques1 === "yes"){
-     results.text("You should learn Javascript!");
-    }
 
   event.preventDefault();
   });
